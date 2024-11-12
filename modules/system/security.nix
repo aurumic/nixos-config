8137e56fead484765d28251c572b3ec6cd93c314
aurumic <zarlophoenix@gmail.com>
2024-11-12 19:17:59 +0200

    nixfmt pass + minor adjustments all around the board

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -3,5 +3,12 @@
 {
   security.sudo.enable = true;
   security.rtkit.enable = true;
-  security.pam.services.hyprlock = { };
+  security.polkit.enable = true;
+
+  security.pam.services = {
+    hyprlock = { };
+    hyprland = {
+      enableGnomeKeyring = true;
+    };
+  };
 }
